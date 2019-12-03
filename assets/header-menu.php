356229007c661159f3c4bbe411aaa61a6dd4c443
jereme <jereme.daniels@gmail.com>
2019-12-03 15:12:11 -0500

    social icons fix

--- assets/header-menu.php
+++ assets/header-menu.php
@@ -5,11 +5,11 @@
         wp_nav_menu( array ( 'theme_location' => 'primary_menu' ) );
     ?>
         <li id=menu-social>
-            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/facebook.svg" alt=""> </a>
-            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/instagram.svg" alt=""></a>
-            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/linkedin.svg" alt=""></a>
-            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/mail.svg" alt=""></a>
-            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/phone.svg" alt=""></a>
+            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/facebook.svg" alt="social icon"> </a>
+            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/instagram.svg" alt="social icon"></a>
+            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/linkedin.svg" alt="social icon"></a>
+            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/mail.svg" alt="social icon"></a>
+            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/phone.svg" alt="social icon"></a>
         </li>
         <li onclick="burger()">Close</li>
     </ul>
--- footer.php
+++ footer.php
@@ -18,11 +18,11 @@
                         </ul>
                     </div>
                     <div class="footer-social">
-                        <a href="#"><img src="./images/social/facebook.svg" alt=""> </a>
-                        <a href="#"><img src="./images/social/instagram.svg" alt=""></a>
-                        <a href="#"><img src="./images/social/linkedin.svg" alt=""></a>
-                        <a href="#"><img src="./images/social/mail.svg" alt=""></a>
-                        <a href="#"><img src="./images/social/phone.svg" alt=""></a>
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/facebook.svg" alt=""> </a>
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/instagram.svg" alt=""></a>
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/linkedin.svg" alt=""></a>
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/mail.svg" alt=""></a>
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/social/phone.svg" alt=""></a>
                     </div>
                 </div>
                 <div class="footer-developer">
