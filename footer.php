8f910e227e2292160a3bdc912bdbddc3a24f96a6
jereme <jereme.daniels@gmail.com>
2019-12-21 09:02:11 -0500

    seo

--- footer.php
+++ footer.php
@@ -18,11 +18,11 @@
                         </ul>
                     </div>
                     <div class="footer-social">
-                        <a href="#"aria-label="facebook link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/facebook.svg" alt=""> </a>
-                        <a href="#"aria-label="instagram link" ><img src="<?php echo get_template_directory_uri(); ?>/images/social/instagram.svg" alt=""></a>
-                        <a href="#"aria-label="linkedin link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/linkedin.svg" alt=""></a>
-                        <a href="#"aria-label="email link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/mail.svg" alt=""></a>
-                        <a href="#"aria-label="direct dial link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/phone.svg" alt=""></a>
+                        <a href="#"rel="noreferrer noopener"aria-label="facebook link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/facebook.svg" alt=""> </a>
+                        <a href="#"rel="noreferrer noopener"aria-label="instagram link" ><img src="<?php echo get_template_directory_uri(); ?>/images/social/instagram.svg" alt=""></a>
+                        <a href="#"rel="noreferrer noopener"aria-label="linkedin link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/linkedin.svg" alt=""></a>
+                        <a href="#"rel="noreferrer noopener"aria-label="email link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/mail.svg" alt=""></a>
+                        <a href="#"rel="noreferrer noopener"aria-label="direct dial link"><img src="<?php echo get_template_directory_uri(); ?>/images/social/phone.svg" alt=""></a>
                     </div>
                 </div>
                 <div class="footer-developer">
